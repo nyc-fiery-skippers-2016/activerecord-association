7a036101a42059a9b374e7f45c18bbfc08b98c86
Torey Hickman <torey@toreyhickman.com>
2015-07-02 17:16:06 -0400

    Update specs

--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -1,44 +1,34 @@
 require_relative "../spec_helper"
 
 describe Hotel do
+  describe 'associations' do
+    before(:each) do
+      @ritz   = Hotel.create!(name: "Ritz")
+      @westin = Hotel.create!(name: "Westin")
 
-  before(:all) do
-    DatabaseCleaner.start
+      @susan = User.create!(name: "Susan Smith")
+      @sam   = User.create!(name: "Sam Smith")
+      @joe   = User.create!(name: "Joe Schmo")
 
-    @ritz   = Hotel.create!(name: "Ritz")
-    @westin = Hotel.create!(name: "Westin")
+      @room_1_at_ritz = Room.create!(rate: 300, number: 1, hotel_id: @ritz.id)
+      @room_2_at_ritz = Room.create!(rate: 300, number: 2, hotel_id: @ritz.id)
+      @room_at_westin = Room.create!(rate: 200, number: 1, hotel_id: @westin.id)
 
-    @susan = User.create!(name: "Susan Smith")
-    @sam   = User.create!(name: "Sam Smith")
-    @joe   = User.create!(name: "Joe Schmo")
+      @ritz_booking_1 = Booking.create!(guest_id: @susan.id, room_id: @room_1_at_ritz.id)
+      @ritz_booking_2 = Booking.create!(guest_id: @sam.id, room_id: @room_2_at_ritz.id)
+      @westin_booking = Booking.create!(guest_id: @sam.id, room_id: @room_at_westin.id)
+    end
 
-    @room_1_at_ritz = Room.create!(rate: 300, number: 1, hotel_id: @ritz.id)
-    @room_2_at_ritz = Room.create!(rate: 300, number: 2, hotel_id: @ritz.id)
-    @room_at_westin = Room.create!(rate: 200, number: 1, hotel_id: @westin.id)
+    it "has rooms" do
+      expect(@ritz.rooms).to match_array [@room_1_at_ritz, @room_2_at_ritz]
+    end
 
-    @ritz_booking_1 = Booking.create!(guest_id: @susan.id, room_id: @room_1_at_ritz.id)
-    @ritz_booking_2 = Booking.create!(guest_id: @sam.id, room_id: @room_2_at_ritz.id)
-    @westin_booking = Booking.create!(guest_id: @sam.id, room_id: @room_at_westin.id)
-  end
+    it "has bookings" do
+      expect(@ritz.bookings).to match_array [@ritz_booking_1, @ritz_booking_2]
+    end
 
-  after(:all) do
-    DatabaseCleaner.clean
-  end
-
-
-  it "has a name" do
-    expect(@ritz.name).to eq "Ritz"
-  end
-
-  it "has rooms" do
-    expect(@ritz.rooms).to match_array [@room_1_at_ritz, @room_2_at_ritz]
-  end
-
-  it "has bookings" do
-    expect(@ritz.bookings).to match_array [@ritz_booking_1, @ritz_booking_2]
-  end
-
-  it "has booked guests" do
-    expect(@ritz.booked_guests).to match_array [@susan, @sam]
+    it "has booked guests" do
+      expect(@ritz.booked_guests).to match_array [@susan, @sam]
+    end
   end
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,36 +1,23 @@
 require_relative "../spec_helper"
 
 describe Room do
+  describe 'associations' do
+    before(:each) do
+      @ritz   = Hotel.create!(name: "Ritz")
 
-  before(:all) do
-    DatabaseCleaner.start
+      @susan = User.create!(name: "Susan Smith")
 
-    @ritz   = Hotel.create!(name: "Ritz")
+      @room_at_ritz = Room.create!(rate: 300, number: 1, hotel_id: @ritz.id)
 
-    @susan = User.create!(name: "Susan Smith")
+      @ritz_booking = Booking.create!(guest_id: @susan.id, room_id: @room_at_ritz.id)
+    end
 
-    @room_at_ritz = Room.create!(rate: 300, number: 1, hotel_id: @ritz.id)
+    it "has a hotel" do
+      expect(@room_at_ritz.hotel).to eq @ritz
+    end
 
-    @ritz_booking = Booking.create!(guest_id: @susan.id, room_id: @room_at_ritz.id)
-  end
-
-  after(:all) do
-    DatabaseCleaner.clean
-  end
-
-  it "has a rate" do
-    expect(@room_at_ritz.rate).to eq 300
-  end
-
-  it "has a number" do
-    expect(@room_at_ritz.number).to eq 1
-  end
-
-  it "has a hotel" do
-    expect(@room_at_ritz.hotel).to eq @ritz
-  end
-
-  it "has bookings" do
-    expect(@room_at_ritz.bookings).to match_array [@ritz_booking]
+    it "has bookings" do
+      expect(@room_at_ritz.bookings).to match_array [@ritz_booking]
+    end
   end
 end
