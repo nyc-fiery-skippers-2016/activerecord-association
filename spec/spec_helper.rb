4330d92819ff19d24312a60c223dde5a5510ff97
Torey Hickman <torey@toreyhickman.com>
2015-07-02 16:25:57 -0400

    Remove source/ and update skeleton

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,19 @@
+# Set variable so that we connect
+# to the test database.
+ENV['AR_ENV'] = 'test'
+
 require_relative '../config/environment'
+
 require 'rspec'
 require 'database_cleaner'
 
-I18n.enforce_available_locales = false
-ActiveRecord::Base.logger = nil
 
 RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
   end
+
+  config.before(:each) { DatabaseCleaner.start }
+  config.after(:each)  { DatabaseCleaner.clean }
 end
